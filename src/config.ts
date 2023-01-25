e041284e6ba77dc3af204c5dfa6ea1e455fa89bb
Heisenberg <buffer_finance@protonmail.com>
2023-01-25 20:09:00 +0530

    contracts changed

--- src/config.ts
+++ src/config.ts
@@ -1,13 +1,12 @@
 export enum State {
-  active = 1,
-  exercised = 2,
-  expired = 3,
-  queued = 4,
-  cancelled = 5,
-  opened = 6
+    active = 1,
+    exercised = 2,
+    expired = 3,
+    queued = 4,
+    cancelled = 5,
+    opened = 6
 }
 
-export const RouterAddress = "0x8e012a532bEbD89CC7b9b8B3Fd2a66605038F22D"
-export const USDC = "0x658e6B62e7ab1d2B29a08F85f8442edEed562b48"
-export const BFR = "0x89fEF05446aEA764C53a2f09bB763876FB57ea8E"
-
+export const RouterAddress = "0x32dc3Ce289ef1a600953d8702A827ABDF10081dC";
+export const USDC = "0x658e6B62e7ab1d2B29a08F85f8442edEed562b48";
+export const BFR = "0x89fEF05446aEA764C53a2f09bB763876FB57ea8E";
