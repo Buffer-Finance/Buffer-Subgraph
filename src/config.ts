4ba09e87cc6e053f2cff094e814cff4c2363f9a7
Heisenberg <buffer_finance@protonmail.com>
2023-01-27 17:19:45 +0530

    mainnet deployment

--- src/config.ts
+++ src/config.ts
@@ -1,13 +1,12 @@
 export enum State {
-  active = 1,
-  exercised = 2,
-  expired = 3,
-  queued = 4,
-  cancelled = 5,
-  opened = 6
+    active = 1,
+    exercised = 2,
+    expired = 3,
+    queued = 4,
+    cancelled = 5,
+    opened = 6
 }
 
-export const RouterAddress = "0x8e012a532bEbD89CC7b9b8B3Fd2a66605038F22D"
-export const USDC = "0x658e6B62e7ab1d2B29a08F85f8442edEed562b48"
-export const BFR = "0x89fEF05446aEA764C53a2f09bB763876FB57ea8E"
-
+export const RouterAddress = "0xB25Fe2Ede3145bd0140098c5923eb0Fd377ecEbb";
+export const USDC = "0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8";
+export const BFR = "0x1A5B0aaF478bf1FDA7b934c76E7692D722982a6D";
