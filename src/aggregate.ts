c544c5948131744b06c25fc3570b54e1176eaa65
Tesla <tesla@buffer.finance>
2023-04-20 14:52:00 +0530

    Fix

--- src/aggregate.ts
+++ src/aggregate.ts
@@ -8,31 +8,29 @@ const FACTOR_OF_6 = BigInt.fromI64(1000000);
 const ZERO = BigInt.fromI32(0);
 const FACTOR_OF_2 = BigInt.fromI32(100);
 
-function getLbfrAlloted(
-  userVolume: BigInt,
-): BigInt {
-  let lbfrAlloted = ZERO;
-  
-  let newSlabIndex = ZERO;
+function getLbfrAlloted(userVolume: BigInt): BigInt {
+  let newSlabIndex = 0;
 
   for (let i = 0; i < Slabs.length; i++) {
     let currentSlab = Slabs[i];
     // let currentMinVolume = currentSlab[0];
     if (userVolume > currentSlab[0].times(FACTOR_OF_18)) {
       newSlabIndex = i;
-    }
-    else {
+    } else {
       break;
     }
   }
 
   let newSlab = Slabs[newSlabIndex];
-  // newSlab[0] = minRequiredVolume 
-  // newSlab[1] = LBFR/usdcVolume rate 
+  // newSlab[0] = minRequiredVolume
+  // newSlab[1] = LBFR/usdcVolume rate
   // newSlab[2] = equivalent lbfr before this slab
   // TODO: adjust the units, multipliers below
   return newSlab[2].times(FACTOR_OF_18).plus(
-    userVolume.minus(newSlab[0].times(FACTOR_OF_18)).times(newSlab[1]).div(FACTOR_OF_2)
+    userVolume
+      .minus(newSlab[0].times(FACTOR_OF_18))
+      .times(newSlab[1])
+      .div(FACTOR_OF_2)
   );
 }
 
@@ -70,7 +68,9 @@ export function updateLBFRStats(
   LBFRStat.volume = finalVolume;
   TotalLBFRStat.volume = TotalLBFRStat.volume.plus(totalFee);
 
-  let lbfrAlloted = getLbfrAlloted(finalVolume).minus(getLbfrAlloted(initialVolume));
+  let lbfrAlloted = getLbfrAlloted(finalVolume).minus(
+    getLbfrAlloted(initialVolume)
+  );
   LBFRStat.lBFRAlloted = LBFRStat.lBFRAlloted.plus(lbfrAlloted);
   TotalLBFRStat.lBFRAlloted = TotalLBFRStat.lBFRAlloted.plus(lbfrAlloted);
 
