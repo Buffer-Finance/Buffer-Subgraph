3269fc9b0372d3104af78142726faabd6ca188e7
Tesla <tesla@buffer.finance>
2023-04-24 13:48:04 +0530

    Fix

--- src/convertARBToUSDC.ts
+++ src/convertARBToUSDC.ts
@@ -2,6 +2,9 @@ import { UNISWAP_POOL_CONTRACT } from "./config";
 import { UniswapPool } from "../generated/UniswapPool/UniswapPool";
 import { BigInt, Address } from "@graphprotocol/graph-ts";
 
+const FACTOR_OF_18 = BigInt.fromI64(1000000000000000000);
+const FACTOR_OF_6 = BigInt.fromI64(1000000);
+
 export function convertARBToUSDC(amount: BigInt): BigInt {
 //   let uniswapPool = UniswapPool.bind(Address.fromString(UNISWAP_POOL_CONTRACT));
 //   let slot0_data = uniswapPool.slot0();
@@ -9,6 +12,6 @@ export function convertARBToUSDC(amount: BigInt): BigInt {
 //     .pow(2)
 //     .times(amount)
 //     .div(BigInt.fromI32(2).pow(192));
-  let arbToUSDC = amount;
+  let arbToUSDC = amount.div(FACTOR_OF_18).times(FACTOR_OF_6);
   return arbToUSDC;
 }
